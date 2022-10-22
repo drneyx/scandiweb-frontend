8ea08934b52f02563bf4e9d91e19fc614516c3a6
Nathani Luving <cosmasnathan9@gmail.com>
2022-10-22 12:45:44 +0300

    Add css and scss

--- src/pages/productList.js
+++ src/pages/productList.js
@@ -14,7 +14,7 @@ function ProductList(){
                                 <div className="card product-box">
                                     <div className="d-flex align-items-center justify-content-center inner-box">
                                         <div className="form-check mt-2 check-form">
-                                            <input className="form-check-input" type="checkbox" id="delete-checkbox" value="" id="flexCheckDefault"/>
+                                            <input className="form-check-input" type="checkbox" id="delete-checkbox" value=""/>
                                         </div>
                                         <div className="d-flex align-items-sm-start justify-content-md-start my-4 flex-column ">
                                             <small className="card-text my-0">165 views - 58 hours ago</small>
